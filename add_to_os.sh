17f5767e5f4e1cfb1b3577a0d0b2d867b02c380c
Mathew Thompson <mathewthompson@Mathews-MacBook-Air.local>
2026-06-12 17:02:24 -0500

    removed unnecessary file

--- add_to_os.sh
+++
@@ -1,7 +0,0 @@
-echo "Adding the opencode command to the OS..."
-echo "Run this command after you have run 'su <admin user>' and 'sudo su' from a standard account"
-
-printf "%s " "Press enter to continue or Ctrl+C to cancel..."
-read ans
-
-cp dist/opencode.js /usr/local/bin/opencode
